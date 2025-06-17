495a8e7f344c569af12e6277c4a60ee4b29fb15a
StepanidaKirillina1 <steshakirillina@yandex.ru>
2025-06-17 22:05:14 +0900

    update the order of the arguments

--- src/test/java/FindIntegralTest.java
+++ src/test/java/FindIntegralTest.java
@@ -6,8 +6,8 @@ public class FindIntegralTest {
 
    @Test
     public void testFindIntegral() {
-       assertEquals("1x^3", FindIntegral.integrate(3,2));
-       assertEquals("2x^6", FindIntegral.integrate(12,5));
-       assertEquals("10x^2", FindIntegral.integrate(20,1));
+       assertEquals(FindIntegral.integrate(3,2), "1x^3");
+       assertEquals(FindIntegral.integrate(12,5), "2x^6");
+       assertEquals(FindIntegral.integrate(20,1), "10x^2");
    }
 }
