2c6e2a27e60822be95259e89b2c70f6d1026ee05
StepanidaKirillina1 <steshakirillina@yandex.ru>
2025-06-18 10:18:48 +0900

    add tests

--- src/test/java/FindIntegralTest.java
+++ src/test/java/FindIntegralTest.java
@@ -9,5 +9,7 @@ public class FindIntegralTest {
        assertEquals(FindIntegral.integrate(3,2), "1x^3");
        assertEquals(FindIntegral.integrate(12,5), "2x^6");
        assertEquals(FindIntegral.integrate(20,1), "10x^2");
+       assertEquals(FindIntegral.integrate(40,3), "10x^4");
+       assertEquals(FindIntegral.integrate(90,2), "30x^3");
    }
 }
